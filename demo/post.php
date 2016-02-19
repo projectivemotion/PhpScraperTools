8a87ddebac234e769bba23b7e2d5d7536951f2f1
Amado Martinez <amado@projectivemotion.com>
2016-02-19 01:57:33 -0600

    Initial commit.

---
+++ demo/post.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Project: PhpScraperTools
+ *
+ * @author Amado Martinez <amado@projectivemotion.com>
+ */
+
+require '../src/BaseScraper.php';
+
+/**
+ * Example how to send post data
+ * 
+ */
+
+$Scraper    =   new \projectivemotion\BaseScraper();
+
+$response   =   $Scraper->cache_get('http://posttestserver.com/post.php?dir=projectivemotion/PhpScraperTools',
+        // post data
+      array('username' => 'amado',
+            'password'  =>  'batteryhorsestaple'));
+
+echo $response;
